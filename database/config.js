fa10b6baf1c07737006fe27711f30ce0e9f520a0
putrahxsting <putrahxsting@gmail.com>
2026-01-22 11:50:42 +0700

    Delete database directory

--- database/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  tokens: "8467646495:AAEl135AL_8bX6jA1-l6bgQmT4xavlSs4so",  // Ubah Jadi Token Bot Mu !!!
-  owner: "8100399804", // Ubah Jadi Id Mu !!!
-  port: "5352", // Ubah Jadi Port Panel Mu !!!
-  ipvps: "http://irenggggg.pterodactyl-panel.web.id" // Ubah Jadi Ip Vps Mu !!!
-};
